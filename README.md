268cf6cec6af769e5a7bc3e2fd7b40ecb8584756
Vincent Zauhar <v.zauhar@bell.net>
2024-02-22 19:19:30 -0500

    Added script, updated README

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # Sononymefel
-A small Python script to automatically tag your sample library in the FL Studio browser using Sononym's local database file.
+Sononymefel is a small Python script that automatically creates and assigns tags to your sample library in the FL Studio browser using Sononym's local database file.
+
+## Requirements
+- [Python](https://www.python.org/downloads/)
+- An existing [Sononym](https://www.sononym.net/) library
+- [FL Studio](https://www.image-line.com/)
+
+## Usage
+
+The syntax goes as follows:
+
+`Sononymefel.py dbfile_path [--fl_user_data_folder]`
+
+## How does this work?
+
+### Sononym's database file
+Upon scanning a sample library and automatically tagging the audio files using machine learning, Sononym creates a database file called **sononym.db**, normally located at the root of the imported sample library folder.
+
+Fortunately this database is not encrypted, so we can easily read and extract its contents. For the purposes of this script, we simply need the **filename** and **categories_VS** clumns.
+
+### FL Studio's "Tags" File
+To store the list of tags and to remember which tag(s) are associated with each sample, FL Studio creates a file called **Tags** in the following location:
+
+`USER_DATA_FOLDER/FL Studio/Settings/Browser/`.
+
+Although I found that FL Studio can be very picky about the syntax when parsing this file, we can see that it is relatively straight forward to understand how the information is structured.
---
+++ Sononymefel.py
@@ -0,0 +1,142 @@
+import argparse
+import os
+import sqlite3
+import ast
+import re
+
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+def clear_screen():
+    if os.name == 'nt':
+        _ = os.system('cls')
+    else:
+        _ = os.system('clear')
+
+
+parser = argparse.ArgumentParser(description='Sononymefel - A script to automatically tag your sample library in FL Studio using Sononym\'s database.')
+parser.add_argument('dbfile_path', metavar='dbfile_path', type=str, help='The path to the Sononym database file. It is normally called "sononym.db" and should be at the root of your sample library.')
+parser.add_argument('--fl_user_data_folder', metavar='', type=str, default=os.path.join(os.path.expanduser('~/Documents/Image-Line')),
+                    help='The path to the FL Studio User Data folder. If you have not changed it, it is located in Documents\\Image-Line. This default location will be chosen if this argument is not provided.')
+
+args = parser.parse_args()
+
+
+
+def get_user_confirmation():
+    print(bcolors.WARNING + "WARNING: This operation will overwrite your existing tags.\nPlease make a backup of the Tags file if you have existing tags in case something goes wrong!\n" + bcolors.ENDC)
+    while True:
+        user_input = input("Are you sure you want to proceed? [y/n]: ").lower()
+        if user_input in ['y', 'yes']:
+            return True
+        elif user_input in ['n', 'no']:
+            return False
+        else:
+            print("Invalid input. Please type 'y' or 'yes' to continue, 'n' or 'no' to abort.")
+
+
+def flatten(lst):
+    flat_list = []
+    for item in lst:
+        if isinstance(item, list):
+            flat_list.extend(flatten(item))
+        else:
+            flat_list.append(item)
+    return flat_list
+
+
+
+
+clear_screen()
+print(bcolors.OKBLUE + "=======================" + bcolors.ENDC)
+print(bcolors.OKBLUE + "Welcome to Sononymefel!" + bcolors.ENDC)
+print(bcolors.OKBLUE + "=======================\n" + bcolors.ENDC)
+
+print(bcolors.UNDERLINE + "Sononym database file location:" + bcolors.ENDC + "        " + args.dbfile_path)
+print(bcolors.UNDERLINE + "FL Studio User Data folder location:" + bcolors.ENDC + "   " + args.fl_user_data_folder + "\n")
+
+
+if (get_user_confirmation()):
+    con = sqlite3.connect(args.dbfile_path)
+
+    table_name = 'assets'
+
+    # Fetch the rows
+    query = f'SELECT filename, categories_VS FROM {table_name}' 
+
+    cur = con.cursor()
+    cur.execute(query)
+    print("\n\nFetching database...")
+    rows = cur.fetchall()
+
+
+    # Fetch the list of tags
+    query = f'SELECT DISTINCT categories_VS FROM {table_name}'
+    cur.execute(query)
+    tags = cur.fetchall()
+
+    # Flatten the list since columns contain arrays
+    flat_list = flatten([ast.literal_eval(tag[0]) for tag in tags if tag[0]])
+
+    # Get unique values
+    tag_list = set(flat_list)
+
+    con.close()
+
+
+    print("Writing tags file for the FL Studio Browser...")
+    tags_file = open(args.fl_user_data_folder + '/FL Studio/Settings/Browser/Tags', 'w', encoding='utf-8')
+
+    words_to_eliminate = {'Perc ':'', 'XFX ':'', 'Tone ':''}
+
+    tags_row = '@TagCase=*'
+    for tag in tag_list:
+        # Eliminate tag families
+        for key, value in words_to_eliminate.items():
+            tag = tag.replace(key, value)
+        # Only put quotes around tags that have spaces in them
+        if ' ' in tag:
+            tags_row += ",\"" + tag + "\""
+        else:
+            tags_row += "," + tag
+
+    tags_file.write(tags_row + "\n")
+
+    for row in rows:
+        # Trim the beginning and end
+        row = str(row)[2:-3]
+        for key, value in words_to_eliminate.items():
+            row = row.replace(key, value)
+        
+        row = row.replace("/","\\")
+
+        
+        if "', '[" in row:
+            tags = row.split("', '[", 1)[1]
+            path = row.split("', '[", 1)[0]
+        if  "\", '[" in row:
+            tags = row.split("\", '[", 1)[1]
+            path = row.split("\", '[", 1)[0]
+
+        tags = new_s = re.sub(r'\"(\w+)\"', r'\1', tags).lower()
+        row = "\"f:\\music\\sound libraries\\samples\\" + path + '\",' + tags + "\n"
+
+        tags_file.write(row)
+
+
+    tags_file.close()
+
+    print(bcolors.OKGREEN + "\n\nTags file written successfully!\n" + bcolors.ENDC)
+    input("Press any key to continue...")
+
+else:
+    print(bcolors.FAIL + "\nAborting..." + bcolors.ENDC)
